8ff6221612927bb231a8054ce7a55a9695ad38b2
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-07-31 15:31:02 +1000

    created django apps and py files ready for the development of the SIEM

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = {'name': 'Jon Snow', 'age': 34}
+    return Response(person)
