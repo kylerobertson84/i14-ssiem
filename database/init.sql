0e43fb719ac947d03c3e07f7dfd44c5c704d14d6
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-02 23:13:39 +1000

    fixed up table errors, placed tables in order of dependency

--- database/init.sql
+++ database/init.sql
@@ -3,92 +3,117 @@ CREATE DATABASE IF NOT EXISTS siem_db;
 
 USE siem_db;
 
--- CREATE TABLE Bronze_Event_Data
--- (
-    
--- )
-
-
-CREATE TABLE IF NOT EXISTS Alert
-(
-    'Alert_Id' INT AUTO_INCREMENT PRIMARY KEY,
-    'Timestamp' TIMESTAMP,
-    'Event_id' INT NOT NULL,
-    'Status' ENUM('Investigated', 'Not_Investigated') NOT NULL,
-    'Rule_Id' INT NOT NULL, 
-    FOREIGN KEY (Event_Id) REFFERENCES Event_Data(Event_Id),
-); 
-
-CREATE TABLE IF NOT EXISTS Assigned_Alert
-(
-    'User_Name' VARCHAR(15) NOT NULL,
-    'Alert_Id' INT NOT NULL,
-    FOREIGN KEY (User_Name) REFFERENCES User(User_Name),
-    FOREIGN KEY (Alert_Id) REFFERENCES Alert(Alert_Id),
-); 
+CREATE TABLE IF NOT EXISTS Bronze_Event_Data(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    EventTime VARCHAR(255) NOT NULL,
+    Hostname VARCHAR(255) NOT NULL,
+    Keywords VARCHAR(255) NOT NULL,
+    EventType VARCHAR(255) NOT NULL, 
+    SeverityValue INT NOT NULL, 
+    Severity VARCHAR(255) NOT NULL, 
+    EventID INT NOT NULL, 
+    SourceName VARCHAR(255) NOT NULL, 
+    ProviderGuid VARCHAR(255) NOT NULL, 
+    Version_ INT NOT NULL, 
+    Task INT NOT NULL,
+    OpcodeValue INT NOT NULL,
+    RecordNumber INT NOT NULL,
+    ProcessID INT NOT NULL,
+    ThreadID INT NOT NULL,
+    Channel VARCHAR(255) NOT NULL,
+    Domain VARCHAR(255) NOT NULL,
+    AccountName VARCHAR(255) NOT NULL,
+    UserID VARCHAR(255) NOT NULL,
+    AccountType VARCHAR(255) NOT NULL,
+    Message_ VARCHAR(255) NOT NULL,
+    Opcode VARCHAR(255) NOT NULL,
+    EventReceivedTime VARCHAR(255) NOT NULL,
+    SourceModuleName VARCHAR(255) NOT NULL,
+    SourceModuleType VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Employee
 (
-    'Employee_Id' VARCHAR(10) NOT NULL PRIMARY KEY,
-    'First_Name' VARCHAR(20) NOT NULL,
-    'Last_Name' VARCHAR(20) NOT NULL,
-    'email' VARCHAR(80),
-);
-
-CREATE TABLE IF NOT EXISTS Event_Data
-(
-    'Event_Id' INT AUTO_INCREMENT Primary Key,
-    'Timestamp' TIMESTAMP,
-    'Source_Id' INT NOT NULL,
-    'Rule_Id' TEXT NOT NULL,
-    FOREIGN KEY (Source_Id) REFFERENCES Bronze_Event_Data(External_Id),
-    FOREIGN KEY (Rule_Id) REFFERENCES Rules(Rule_Id), 
-);
-
-CREATE TABLE IF NOT EXISTS Incident_Report
-(
-    'Delivery_id' INT AUTO_INCREMENT PRIMARY KEY,
-    'Type' VARCHAR(20) NOT NULL,
-    'Status' ENUM('ongoing', 'closed') NOT NULL,
-    'Source_Id' INT NOT NULL,
-    'Rule_Id' TEXT,
-    'User_Id' VARCHAR(15) NOT NULL,
-    'Description' TEXT,
-);
+    Employee_Id VARCHAR(10) NOT NULL PRIMARY KEY,
+    First_Name VARCHAR(20) NOT NULL,
+    Last_Name VARCHAR(20) NOT NULL,
+    email VARCHAR(80)
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Role
 (
-    'Role_Id' VARCHAR(10) PRIMARY KEY,
-    "Name" VARCHAR(20)
-);
-
-CREATE TABLE IF NOT EXISTS Role_Permission
-(
-    'Role_Permission_Id' VARCHAR(10) PRIMARY KEY,
-    'Permission_Id' VARCHAR(10),
-    FOREIGN KEY (Permission_Id) REFFERENCES Permission(Permission_Id),
-);
+    Role_Id VARCHAR(10) PRIMARY KEY,
+    Name VARCHAR(20)
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Rules
 (
-    'Rule_Id' INT AUTO_INCREMENT PRIMARY KEY,
-    'Rule_Name' VARCHAR(30),
-    'Description' VARCHAR(255),
-    'Conditions' TEXT,
-)
+    Rule_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Rule_Name VARCHAR(30),
+    Description VARCHAR(255),
+    Conditions TEXT
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS Permission
+CREATE TABLE IF NOT EXISTS Event_Data
 (
-    'Permission_Id' VARCHAR(10) PRIMARY KEY,
-    'Name' VARCHAR(20),
-);
+    Event_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Time_stamp TIMESTAMP,
+    Source_Id INT NOT NULL,
+    Rule_Id INT NOT NULL,
+    FOREIGN KEY (Source_Id) REFERENCES Bronze_Event_Data(id),
+    FOREIGN KEY (Rule_Id) REFERENCES Rules(Rule_Id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Alert
+(
+    Alert_Id INT AUTO_INCREMENT PRIMARY KEY,
+    Time_stamp TIMESTAMP,
+    Event_id INT NOT NULL,
+    _Status ENUM('Investigated', 'Not_Investigated') NOT NULL,
+    Rule_Id INT NOT NULL, 
+    FOREIGN KEY (Event_Id) REFERENCES Event_Data(Event_Id)
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS User
 (
-    'User_Id' VARCHAR(15) Primary Key,
-    'User_Name' VARCHAR(15),
-    'Password' VARCHAR(255),
-    'Role_Id' VARCHAR(15),
-    FOREIGN KEY (Role_Id) REFFERENCES Role(Role_Id),
-);
+    User_Id VARCHAR(15) Primary Key,
+    User_Name VARCHAR(15),
+    _password VARCHAR(255),
+    Role_Id VARCHAR(10),
+    FOREIGN KEY (Role_Id) REFERENCES Role(Role_Id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Assigned_Alert
+(
+    User_Id VARCHAR(15) NOT NULL,
+    Alert_Id INT NOT NULL,
+    FOREIGN KEY (User_Id) REFERENCES User(User_Id),
+    FOREIGN KEY (Alert_Id) REFERENCES Alert(Alert_Id)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Incident_Report
+(
+    Delivery_id INT AUTO_INCREMENT PRIMARY KEY,
+    Type VARCHAR(20) NOT NULL,
+    Status ENUM('ongoing', 'closed') NOT NULL,
+    Source_Id INT NOT NULL,
+    Rule_Id TEXT,
+    User_Id VARCHAR(15) NOT NULL,
+    _description TEXT
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Permission
+(
+    Permission_Id VARCHAR(10) PRIMARY KEY,
+    _Name VARCHAR(20)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Role_Permission
+(
+    Role_Permission_Id VARCHAR(10) PRIMARY KEY,
+    Permission_Id VARCHAR(10),
+    FOREIGN KEY (Permission_Id) REFERENCES Permission(Permission_Id)
+) ENGINE=InnoDB;
+
+
 
