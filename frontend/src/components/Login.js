78eedf0c9373269a07b637483afb0edcab09eb1b
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-05 08:39:07 +1000

    1. built login component based off prototype. 2. got frontend user auth ready to connect to backend JWT

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,31 +1,95 @@
 
+
+
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const defaultTheme = createTheme();
+
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:8000/api/token/', { username, password });
-      localStorage.setItem('token', response.data.access);
-    } catch (error) {
-      console.error('Error logging in', error);
-    }
-  };
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:8000/api/token/', { username, password });
+            localStorage.setItem('token', response.data.access);
+        } catch (error) {
+            console.error('Error logging in', error);
+        }
+    };
+
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+
+                    <Typography component="h1" variant="h2">
+                        Simple SIEM
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="user"
+                            label="Username"
+                            name="user"
+                            autoComplete="user"
+                            autoFocus
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Login
+                        </Button>
+
+                    </Box>
+                </Box>
+
+            </Container>
+        </ThemeProvider>
+    );
+}
 
 export default Login;
+
