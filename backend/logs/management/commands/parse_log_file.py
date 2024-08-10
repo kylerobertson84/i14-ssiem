b109199d218edbf516cf8ae7fcc346f8550cb428
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-10 11:16:15 +1000

    able to get log_parsing command to work

--- backend/logs/management/commands/parse_log_file.py
+++ backend/logs/management/commands/parse_log_file.py
@@ -24,14 +24,17 @@ def parse_line(line):
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('log1.txt', type=str, help='The path to the log file')
+        parser.add_argument('logfile', type=str, help='The path to the log file')
 
     def handle(self, *args, **kwargs):
-        logfile = kwargs['log1.txt']
-        with open(logfile, 'r') as file:
-            for line in file:
-                parse_line(line)
-        self.stdout.write(self.style.SUCCESS('Successfully parsed the log file'))
+        logfile = kwargs['logfile']
+        try:
+            with open(logfile, 'r') as file:
+                for line in file:
+                    parse_line(line)
+            self.stdout.write(self.style.SUCCESS('Successfully parsed the log file'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f'File not found: {logfile}'))
 
 
     
