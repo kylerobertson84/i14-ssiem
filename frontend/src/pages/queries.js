d0fcde40777a371a0a9e04e78ebf027d9058e03e
Ramj619 <89727018+Ramj619@users.noreply.github.com>
2024-08-10 12:21:47 +1000

    Query Pages V1
    Changed from running from App.js to now it's own page which is called into App.js instead.

--- frontend/src/pages/queries.js
+++ frontend/src/pages/queries.js
@@ -1,12 +1,31 @@
-
-
-import React from 'react';
+import React, { useState } from 'react';
+import SearchBar from './Components/SearchBar'
+import SearchTable from './Components/SearchTable'
 
 const Queries = () => {
+ const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (query) => {
+    setSearchResults([query]);
+  };
+
+  const [tableData, setTableData] = useState([
+    // Sample data for demonstration
+    { device: 'Device 1', ip: '192.168.1.1', time: 'Apr 11 12:00 PM', severity: 'High', process: 'Process A' },
+    { device: 'Device 2', ip: '192.168.1.2', time: 'Apr 12 01:00 PM', severity: 'Medium', process: 'Process B' },
+    { device: 'Device 3', ip: '192.168.1.3', time: 'Apr 13 02:00 PM', severity: 'Low', process: 'Process C' },
+  ]);
+
   return (
-    <div>
-      <h1>Queries Page</h1>
-      
+    <div className="app-container">
+      <h1>Queries</h1>
+      <SearchBar onSearch={handleSearch} />
+      <div className="results-container">
+      </div>
+      <div className="table-container">
+        <h2>Data Table:</h2>
+        <SearchTable data={tableData} />
+      </div>
     </div>
   );
 };
