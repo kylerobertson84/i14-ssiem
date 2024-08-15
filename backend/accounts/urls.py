f834bc0eebf2015c065ffc0e6441a67be8dff941
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-16 06:42:51 +1000

    Fixed front end log in issue. Added a bypass for logging in to the frontend with an environment variable.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -12,7 +12,7 @@ router.register(r'permissions', PermissionViewSet)
 router.register(r'role-permissions', RolePermissionViewSet)
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -5,6 +5,12 @@ import AuthService from '../services/AuthService';
 
 const PrivateRoute = () => {
     const user = AuthService.getCurrentUser();
+    const shouldBypassAuth = process.env.REACT_APP_BYPASS_AUTH === 'true';
+
+    if (shouldBypassAuth)
+    {
+        return <Outlet />;
+    }
     return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
