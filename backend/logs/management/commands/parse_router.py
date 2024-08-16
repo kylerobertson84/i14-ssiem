f0509448efed63feb9de4710585a74478e32d223
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-17 07:21:31 +1000

    added in logic to parse the date and time, host name and process from the router log file

--- backend/logs/management/commands/parse_router.py
+++ backend/logs/management/commands/parse_router.py
@@ -3,22 +3,58 @@ from django.core.management.base import BaseCommand
 from logs.models import *
 import re
 
-# def parse_fields():
 
-#     field_dict = dict()
 
-    
+# constant regular expressions
+
+SEVERITY_RE = r'(?<=\>)'
+DATE_TIME_RE = r'(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})'
+
 
 
 def parse_line(line):
 
     try:
 
-        split_sev_from_header = re.split(r'(?<=\>)', line)
+        # to get the severity / priority
+
+        split_sev_from_header = re.split(SEVERITY_RE, line)
         sev_str = split_sev_from_header[0]
         severity = sev_str.replace('<', '').replace('>', '')
 
-        print(severity[0])
+        # print(severity[0])
+        # print(split_sev_from_header[1])
+        
+        # to get the date and time
+
+        split_date_from_header = re.split(DATE_TIME_RE, split_sev_from_header[1])
+        date_time = split_date_from_header[1]
+        # print(date_time)
+
+        # to get host name
+
+        strip_str = split_date_from_header[2].strip()
+        split_host_from_header = strip_str.split()
+        host_name = split_host_from_header[0]
+        # print(host_name)
+
+        # to get process
+
+        check_process = split_host_from_header[1]
+        # print(check_process)
+
+        # check if process contains a comma:
+
+        if "," in check_process:
+            remove_comma = check_process.replace(",", " ", 1)
+            split_process_from_msg = remove_comma.split()
+            process = split_process_from_msg[0]
+        else:
+            process = check_process.replace(":","")
+           
+        
+        print(process)
+
 
         
 
