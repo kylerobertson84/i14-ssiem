275b58647f77d4e77e5877b53198f0c594998807
Manas <manasanantsowale@gmail.com>
2024-08-17 17:52:30 +1000

    Working mode of the report generation.(fixed with Thomas)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
@@ -6,21 +5,26 @@ import Login from './components/Login';
 import Dashboard from './pages/dashboard';
 import Investigations from './pages/investigations';
 import Queries from './pages/queries';
-import ReportsPage from './pages/reports';
+import Reports from './pages/reports';
+import Alerts from './pages/alerts';
 
-const App = () => {
-  return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/investigations" element={<Investigations />} />
-        <Route path="/queries" element={<Queries />} />
-        <Route path="/reports" element={<ReportsPage />} />
-      </Routes>
-    </Router>
-  );
-};
+import PrivateRoute from './components/PrivateRoute';
+import ProtectedLayout from './components/ProtectedLayout';
+
+
+
+
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/investigations" element={<Investigations />} />
+      <Route path="/queries" element={<Queries />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path='/alerts' element={<Alerts />} />
+    </Routes>
+  </Router>
+);
 export default App;
-
