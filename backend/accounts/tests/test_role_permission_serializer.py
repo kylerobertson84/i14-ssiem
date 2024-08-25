929256d922469adb4d9e801ac591e2a34da1b7fe
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-26 08:16:16 +1000

    started creating test cases for role permission serializers, created a test case to check valid role permission creation

---
+++ backend/accounts/tests/test_role_permission_serializer.py
@@ -0,0 +1,21 @@
+
+from rest_framework.test import APITestCase
+from accounts.models import Role, Permission, RolePermission
+from accounts.serializers import RolePermissionSerializer
+
+class RolePermissionSerializerTests(APITestCase):
+
+    def setUp(self):
+        self.role = Role.objects.create(name='Admin')
+        self.permission = Permission.objects.create(name='Can_Edit')
+
+    def test_valid_role_permission_creation(self):
+        data = {
+            'role': str(self.role.role_id),
+            'permission': str(self.permission.permission_id)
+        }
+        serializer = RolePermissionSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        role_permission = serializer.save()
+        self.assertEqual(role_permission.role, self.role)
+        self.assertEqual(role_permission.permission, self.permission)
