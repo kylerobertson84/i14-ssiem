8f45d9304dd1d9bad7c6b7e9752d734568e6e8ab
OliH1337 <167395756+OliH1337@users.noreply.github.com>
2024-08-26 18:07:08 +1000

    Adding ability to search for user's

--- frontend/src/pages/preferences.js
+++ frontend/src/pages/preferences.js
@@ -53,6 +53,9 @@ const Preferences = () => {
         <div>
           <PreferencesForm/>
         </div>
+        <div>
+          <SearchBar/>
+        </div>
       
       </div>
 
@@ -62,5 +65,39 @@ const Preferences = () => {
   export default Preferences;
 
   
-
+  function SearchBar() {
+    const [query, setQuery] = useState ("");
+    const [results, setResults] = useState ([]);
+    return (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+              flexDirection: "column",
+            }}
+          >
+              <div style={{
+              position: "absolute",
+              top: "25%",
+              left: "5%",
+              transform: "translate(-50% )",
+               fontSize: '2em'
+            }}>
+            <span style={ {color: "#4285F4"}}>Search For User</span>
+          </div> 
+            <div
+              style={{
+                position: "absolute",
+                top: "40%",
+                left: "50%",
+                transform: "translate(-50% )",
+              }}
+            >
+                {/*<SearchBar query={query} setQuery={setQuery}/>*/} {/* causes the page not to load*/}
+            </div>
+          </div>   
+    );
+  }
 {/* */}
