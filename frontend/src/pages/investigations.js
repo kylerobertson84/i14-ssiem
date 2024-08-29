730a36bf2a5cbf534fb2c91d0fda54ccf7692453
OliH1337 <167395756+OliH1337@users.noreply.github.com>
2024-08-29 15:46:12 +1000

    Investigation page began
    Began work on creating a table that will store the investigations

--- frontend/src/pages/investigations.js
+++ frontend/src/pages/investigations.js
@@ -1,8 +1,62 @@
 
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/NavBar';
+import { 
+  Container, 
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField
+} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+//import '../Design/Investigation.css'
 
+const alerts = [
+  { id: 1, device: 'Stefan-Laptop', type: 'Failed Login Attempt', status: 'Closed', timestamp: '2024-03-12 16:04:26' },
+  { id: 2, device: 'Stefan-Laptop', type: 'New User Account Created', status: 'In Progress', timestamp: '2024-03-12 16:04:26' },
+  { id: 4, device: 'PC-4', type: 'Failed Login Attempt', status: 'Open', timestamp: '2024-04-12 16:04:26' },
+  { id: 5, device: 'Pc-5', type: 'Windows Defender Detected Malware', status: 'Open', timestamp: '2024-05-12 16:04:26' },
+];
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'device', headerName: 'Device Name', width: 130 },
+  { field: 'type', headerName: 'Type', width: 130 },
+  { field: 'status', headerName: 'Status', width: 130},
+  { field: 'timestamp', headerName: 'TimeStamp', width: 160,},
+];
+
+const Investigations = () => {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Investigations</h1>
+      <Container maxWidth="sm" className="investigation-Table">
+            <DataGrid>
+              rows = {alerts}
+              columns = {columns}
+              initialState={{
+                pagination: {
+                  page: {page},
+                  paginationModel: { page: 0, pageSize: 5 },
+                },
+              }}
+              pageSizeOptions={[5, 10]}
+              checkboxSelection
+              sx={{ overflow: 'clip' }}
+            </DataGrid>
+      </Container>
+  </div>
+  );
+};
+
+export default Investigations;
+
+/*
 const Investigations = () => {
   return (
     <div>
@@ -14,3 +68,4 @@ const Investigations = () => {
 };
 
 export default Investigations;
+*/
