b14aeeec7d8471ecf76d1297e642918765ebd32e
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-08-31 16:19:52 +1000

    cleaned up code ready to merge with develop

--- frontend/src/services/apiConfig.js
+++ frontend/src/services/apiConfig.js
@@ -2,12 +2,12 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL 
 
 const API_ENDPOINTS = {
-    user: `${API_BASE_URL}/users/me/`,
-    logCount: `${API_BASE_URL}/v1/bronze-events/count/`,
-    routerLogCount: `${API_BASE_URL}/v1/router-data/router_log_count/`,
-    log_percentages: `${API_BASE_URL}/v1/log-percentage/log_percentages/`,
-    logs_per_hour: `${API_BASE_URL}/v1/logs-aggregation/logs_per_hour/`,
-    events_today: `${API_BASE_URL}/v1/events-today/events_today/`,
+    user: `${API_BASE_URL}users/me/`,
+    logCount: `${API_BASE_URL}v1/bronze-events/count/`,
+    routerLogCount: `${API_BASE_URL}v1/router-data/router_log_count/`,
+    log_percentages: `${API_BASE_URL}v1/log-percentage/log_percentages/`,
+    logs_per_hour: `${API_BASE_URL}v1/logs-aggregation/logs_per_hour/`,
+    events_today: `${API_BASE_URL}v1/events-today/events_today/`,
     
     // Add more endpoints here as needed
   };
