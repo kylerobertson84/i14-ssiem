5e51d785de097ffe2212b54323dceba9ba8a5bfe
623190x <623190x@student.swin.edu.au>
2024-08-31 21:30:34 +1000

    merged kyles parsing with UPD reciving

--- backend/logs/updparse.py
+++ backend/logs/updparse.py
@@ -15,10 +15,10 @@ from logs.management.commands.constants import *
 
 class SyslogUDPServer:
 
-    def __init__(self, host="0.0.0.0", port=514, output_file="received_syslog.txt"):
+    def __init__(self, host="0.0.0.0", port=514):
         self.host = host
         self.port = port
-        self.output_file = output_file
+       
 
     def start(self):
         # Set up a UDP socket
@@ -26,27 +26,20 @@ class SyslogUDPServer:
         sock.bind((self.host, self.port))
 
         print(f"Listening for UDP syslog messages on {self.host}:{self.port}...")
-        
-        try:
-            with open(self.output_file, 'a') as file:  # Open file in append mode
-                while True:
-                    # Receive syslog packet
-                    data, addr = sock.recvfrom(4096)  # Buffer size of 4096 bytes
-                    syslog_message = data.decode('utf-8').strip()
+        try :    
+            while True:
+                # Receive syslog packet
+                data, addr = sock.recvfrom(4096)  # Buffer size of 4096 bytes
+                syslog_message = data.decode('utf-8').strip()
                     
-                    # Add a timestamp to each message
-                    #formatted_message = f"{timestamp} {syslog_message}"
-
-                    # Print to the terminal
-                    print(f"Received message from {addr}: {syslog_message}")
-                    
-                    # Write to the file
-                    file.write(syslog_message + "\n")
-
+                # Add a timestamp to each message
+                #formatted_message = f"{timestamp} {syslog_message}"
+                insert_data(syslog_message)
+                # Print to the terminal
+                print(f"Received message from {addr}: {syslog_message}")
+    
         except KeyboardInterrupt:
             print("\nServer stopped.")
-        except Exception as e:
-            print(f"Error receiving syslog messages: {e}")
         finally:
             sock.close()
 
@@ -264,5 +257,5 @@ def parse_line(line):
 
 
 if __name__ == "__main__":
-    server = SyslogUDPServer(host="0.0.0.0", port=514, output_file="received_syslog.txt")
+    server = SyslogUDPServer(host="0.0.0.0", port=514)
     server.start()
