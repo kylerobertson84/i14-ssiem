75c8cf3e27fd50cb4a4aef598efef962a2650582
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-09-05 07:39:27 +1000

    fixed a visual bug on preferences page

--- frontend/src/pages/preferences.js
+++ frontend/src/pages/preferences.js
@@ -54,9 +54,9 @@ const Preferences = () => {
       <div>
         <PreferencesForm />
       </div>
-      <div>
+      {/* <div>
         <SearchBar />
-      </div>
+      </div> */}
 
     </div>
 
@@ -66,39 +66,39 @@ const Preferences = () => {
 export default Preferences;
 
 
-function SearchBar() {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
-      }}
-    >
-      <div style={{
-        position: "absolute",
-        top: "25%",
-        left: "5%",
-        transform: "translate(-50% )",
-        fontSize: '2em'
-      }}>
-        <span style={{ color: "#4285F4" }}>Search For User</span>
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "40%",
-          left: "50%",
-          transform: "translate(-50% )",
-        }}
-      >
-        {/*<SearchBar query={query} setQuery={setQuery}/>*/} {/* causes the page not to load*/}
-      </div>
-    </div>
-  );
-}
-{/* */ }
+// function SearchBar() {
+//   const [query, setQuery] = useState("");
+//   const [results, setResults] = useState([]);
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         height: "100vh",
+//         flexDirection: "column",
+//       }}
+//     >
+//       <div style={{
+//         position: "absolute",
+//         top: "25%",
+//         left: "5%",
+//         transform: "translate(-50% )",
+//         fontSize: '2em'
+//       }}>
+//         <span style={{ color: "#4285F4" }}>Search For User</span>
+//       </div>
+//       <div
+//         style={{
+//           position: "absolute",
+//           top: "40%",
+//           left: "50%",
+//           transform: "translate(-50% )",
+//         }}
+//       >
+//         {/*<SearchBar query={query} setQuery={setQuery}/>*/} {/* causes the page not to load*/}
+//       </div>
+//     </div>
+//   );
+// }
+// {/* */ }
