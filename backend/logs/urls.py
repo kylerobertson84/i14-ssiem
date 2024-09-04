1d1b8385e8a4c07f7e72d6722cf2b758fdc49ac7
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-09-05 07:26:43 +1000

    added the routing for the API

--- backend/logs/urls.py
+++ backend/logs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BronzeEventDataViewSet, RouterDataViewSet, LogPercentageViewSet, LogAggregationViewSet, EventsToday
+from .views import BronzeEventDataViewSet, RouterDataViewSet, LogPercentageViewSet, LogAggregationViewSet, EventsToday, HostnameCountViewSet
 
 router = DefaultRouter()
 router.register(r'bronze-events', BronzeEventDataViewSet)
@@ -10,6 +10,7 @@ router.register(r'logs-aggregation', LogAggregationViewSet, basename='logs-aggre
 
 # router.register(r'events', EventDataViewSet)
 router.register(r'events-today', EventsToday, basename='events-today')
+router.register(r'hostname-count', HostnameCountViewSet, basename='hostname-count')
 
 urlpatterns = [
     path('', include(router.urls)),
