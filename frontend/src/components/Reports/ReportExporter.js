bf17ac4a584d9c191b9d5dd8e7909838eb3f875f
623190x <623190x@student.swin.edu.au>
2024-09-17 18:01:05 +1000

    working ssl/tls configuration

--- frontend/src/components/Reports/ReportExporter.js
+++ frontend/src/components/Reports/ReportExporter.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import html2pdf from 'html2pdf.js';
+//import html2pdf from 'html2pdf.js';
 
 const ReportExporter = ({ report }) => {
     const handleExportPDF = () => {
         const element = document.getElementById('report-content');
-        html2pdf().from(element).save(`${report.name}_Report.pdf`);
+       // html2pdf().from(element).save(`${report.name}_Report.pdf`);
     };
 
     return (
