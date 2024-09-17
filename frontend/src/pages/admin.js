525bbfa487469f062aa3f419ff920a2670a3ac70
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-09-17 11:08:14 +1000

    added an admin page to to create new users

---
+++ frontend/src/pages/admin.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import CreateAnalystUserForm from '../components/CreateAnalystUserForm';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <CreateAnalystUserForm />
+    </div>
+  );
+};
+
+export default AdminPage;
