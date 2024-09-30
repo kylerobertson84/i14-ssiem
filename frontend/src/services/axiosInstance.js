ab9dbd5696ad55acb03998953adc06f9c73be4ed
Thomas <vo.thomas.11647@outlook.com>
2024-10-01 00:23:17 +1000

    fxing issue with generating PDF

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,40 +1,38 @@
-
-import axios from 'axios';
-import AuthService from './AuthService';
+import axios from "axios";
+import AuthService from "./AuthService";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true,
+	baseURL: process.env.REACT_APP_API_URL,
+	withCredentials: true,
 });
 
-
 instance.interceptors.request.use(
-    config => {
-        const user = AuthService.getCurrentUser();
-        if (user && user.access) {
-            config.headers.Authorization = 'Bearer ' + user.access;
-        }
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
-    }
+	(config) => {
+		const user = AuthService.getCurrentUser();
+		if (user && user.access) {
+			config.headers.Authorization = "Bearer " + user.access;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 instance.interceptors.response.use(
-    response => response,
-    async error => {
-        const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-            const user = AuthService.getCurrentUser();
-            const newTokens = await AuthService.refreshToken(user.refresh);
-            axios.defaults.headers.common['Authorization'] = 'Bearer ' + newTokens.access;
-            return instance(originalRequest);
-        }
-        return Promise.reject(error);
-    }
+	(response) => response,
+	async (error) => {
+		const originalRequest = error.config;
+		if (error.response.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+			const user = AuthService.getCurrentUser();
+			const newTokens = await AuthService.refreshToken(user.refresh);
+			axios.defaults.headers.common["Authorization"] =
+				"Bearer " + newTokens.access;
+			return instance(originalRequest);
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default instance;
-
