fc3274f723383e56d2c8cabe50817316c62a8f82
Kyle Robertson <robertson.m.kyle@gmail.com>
2024-10-03 08:31:19 +1000

    updated order in backend testing script

--- scripts/run_backend_unit_tests.sh
+++ scripts/run_backend_unit_tests.sh
@@ -1,6 +1,6 @@
 
 docker-compose exec backend python manage.py test accounts.tests
 docker-compose exec backend python manage.py test alerts.tests
-docker-compose exec backend python manage.py test logs.tests
 docker-compose exec backend python manage.py test core.tests
+docker-compose exec backend python manage.py test logs.tests
 docker-compose exec backend python manage.py test reports.tests
